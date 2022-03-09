89b8182ad00a4ec70e34c08f6d9f43fd52ed95c6
lu <10154508169@stu.ecnu.edu.cn>
2022-03-09 02:33:27 +0000

    test main ci.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. (patch)
+          Edit <code>src/App.js</code> and save to reload. (develop)
         </p>
         <a
           className="App-link"
