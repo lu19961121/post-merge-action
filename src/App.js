d441e155f727df5b927cbf3e8cdd799242287077
lu <10154508169@stu.ecnu.edu.cn>
2022-03-09 04:32:02 +0000

    g

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. (f)
+          Edit <code>src/App.js</code> and save to reload. (g)
         </p>
         <a
           className="App-link"
